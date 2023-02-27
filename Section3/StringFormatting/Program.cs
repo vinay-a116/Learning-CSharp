1fdea6eaf0ce2cff39208ea1660953a79708ae6e
ashwathnarayanaswamy.kumar <ashwathnarayanaswamy.kumar@ihis.com.sg>
2023-02-27 17:53:35 +0800

    String Interpolation Formatting

--- Section3/StringFormatting/Program.cs
+++ Section3/StringFormatting/Program.cs
@@ -16,9 +16,15 @@ namespace StringFormatting
             string column2Heading = "Quantity";
             string column3Heading = "Price";
 
-            Console.WriteLine($"{column1Heading, -12} {column2Heading, -8} {column3Heading, -6}");
-            Console.WriteLine($"{apples, -12} {appleQuantity, -8} {applePrice,-6} cents");
-            Console.WriteLine($"{oranges,-12} {orangeQuantity,-8} {orangePrice,-6} cents");
+            Console.WriteLine($"{column1Heading,-12} {column2Heading,-8} {column3Heading,-6}");
+            Console.WriteLine($"{apples,-12} {appleQuantity,-8} {applePrice / 100.0,6:C}");
+            Console.WriteLine($"{oranges,-12} {orangeQuantity,-8} {orangePrice / 100.0,6:C}");
+
+            Console.WriteLine($" PI is {Math.PI}");
+            Console.WriteLine($" PI is {Math.PI:F3}");
+            Console.WriteLine($" PI is {Math.PI:F0}");
+            Console.WriteLine($" PI is {Math.PI:F12}");
+            Console.WriteLine($" PI is {Math.PI:F99}");
         }
     }
 }
